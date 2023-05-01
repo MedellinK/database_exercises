0b887f3b775637aa4c5c7ad11d965330c284b649
Kelvyn Medellin <medellinkelvyn98@gmail.com>
2023-05-01 14:15:17 -0500

    Exercises turn-in

---
+++ Testing123.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE testing123;
+
+show databases;
+
+USE testing123;
+
+DROP DATABASE testing123;
+
+show databases;
+
+DROP database if exists testing123;
+
+CREATE database codeup_test_db;
+
+CREATE USER 'codeup_test_user'@'localhost' IDENTIFIED BY '12345';
+
+GRANT ALL ON codeup_test_db.* TO 'codeup_test_user'@'localhost';
+
+
+
